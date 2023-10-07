af29232730c1101227f82699a5ed00b2828f535d
Erdem Tulu <erdem@edcast.com>
2023-10-07 15:17:28 +0200

    dev: add users and favorites modules init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', loadChildren: () => import('./users/users.module').then((m) => m.UsersModule) },
+  { path: 'favorites', loadChildren: () => import('./favorites/favorites.module').then((m) => m.FavoritesModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
