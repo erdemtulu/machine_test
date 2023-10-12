6c8f1507081628289fab52890b51e290bf1cecae
Erdem Tulu <erdem@edcast.com>
2023-10-12 15:58:37 +0200

    chore: remove unused import

--- src/app/users/state/users.state.ts
+++ src/app/users/state/users.state.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Action, Selector, State, StateContext } from '@ngxs/store';
-import { catchError, lastValueFrom, of, tap } from 'rxjs';
+import { catchError, lastValueFrom, tap } from 'rxjs';
 import { IPageable } from 'src/app/models/core.model';
 import { User } from '../models/user.model';
 import { UsersService } from '../service/users.service';
